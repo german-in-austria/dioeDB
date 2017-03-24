fcc7590b34133a3bfb80ab758a8a90760053058c
hcb79 <hcb@hcb.de>
2017-03-24 13:36:09 +0100

    Anpassung für Tabelle "Orte" um über Openstreetmap auszuwählen!

--- app/DB/forms.py
+++ app/DB/forms.py
@@ -5,28 +5,40 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 from django.db import models
 
 # Spezielles ModelChoiceWidget als PopUp fuer kuerzere Ladezeiten
-class MyModelChoiceWidget(forms.widgets.Select):
+class DioeModelChoiceWidget(forms.widgets.Select):
 	def __init__(self, attrs=None, choices=(), queryset=None):
-		super(MyModelChoiceWidget, self).__init__(attrs)
+		super(DioeModelChoiceWidget, self).__init__(attrs)
 		self.choices = list(choices)
 		self.queryset = queryset
 	def render(self, name, value, attrs=None):
-		atext = '<span class="grey">Keine Eingabe vorhanden</span>'
-		btns = ''
-		if value:
-			atext = '<span title="PK: '+str(value)+'">'+str(self.queryset.get(pk=value))+'</span>'
-			btns+= '<button class="seleobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
-			btns+= '<button class="viewobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in PopUp anzeigen"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>'
-			btns+= '<button class="openobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
+		if self.queryset.model.__name__ == 'tbl_orte':
+			atext = '<span class="grey">Keine Eingabe vorhanden</span>'
+			btns = ''
+			if value:
+			 	atext = '<span title="PK: '+str(value)+'">'+str(self.queryset.get(pk=value))+'</span>'
+			 	btns+= '<button class="seleobjosm" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
+			 	btns+= '<button class="openobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
+			else:
+			 	btns+= '<button class="seleobjosm" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
+			 	btns+= '<button class="openobj hidden" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
+			return '<div class="form-control-static"><input type="hidden" name="'+str(name)+'" value="'+str(value)+'" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-required="'+str(self.is_required)+'">'+atext+btns+'</div>'
 		else:
-			btns+= '<button class="seleobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
-			btns+= '<button class="viewobj hidden" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in PopUp anzeigen"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>'
-			btns+= '<button class="openobj hidden" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
-		return '<div class="form-control-static"><input type="hidden" name="'+str(name)+'" value="'+str(value)+'" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-required="'+str(self.is_required)+'">'+atext+btns+'</div>'
+			atext = '<span class="grey">Keine Eingabe vorhanden</span>'
+			btns = ''
+			if value:
+				atext = '<span title="PK: '+str(value)+'">'+str(self.queryset.get(pk=value))+'</span>'
+				btns+= '<button class="seleobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
+				btns+= '<button class="viewobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in PopUp anzeigen"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>'
+				btns+= '<button class="openobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="'+str(value)+'" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
+			else:
+				btns+= '<button class="seleobj" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in PopUp auswaehlen"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></button>'
+				btns+= '<button class="viewobj hidden" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in PopUp anzeigen"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></button>'
+				btns+= '<button class="openobj hidden" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-obj-pk="0" title="Element in neuen Fenster anzeigen"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></button>'
+			return '<div class="form-control-static"><input type="hidden" name="'+str(name)+'" value="'+str(value)+'" data-appname="'+self.queryset.model._meta.app_label+'" data-tabname="'+self.queryset.model.__name__+'" data-required="'+str(self.is_required)+'">'+atext+btns+'</div>'
 
-# Angepasstes "ModelChoiceField" um "queryset" an "MyModelChoiceWidget" zu uebergeben.
-class MyModelChoiceField(forms.ModelChoiceField):
-	widget = MyModelChoiceWidget
+# Angepasstes "ModelChoiceField" um "queryset" an "DioeModelChoiceWidget" zu uebergeben.
+class DioeModelChoiceField(forms.ModelChoiceField):
+	widget = DioeModelChoiceWidget
 	def __init__(self, queryset, empty_label="---------",
 				required=True, widget=None, label=None, initial=None,
 				help_text='', to_field_name=None, limit_choices_to=None,
@@ -46,7 +58,7 @@ class MyModelChoiceField(forms.ModelChoiceField):
 		self.widget.queryset = queryset
 		self.widget.choices = self.choices
 
-forms.ModelChoiceField = MyModelChoiceField
+forms.ModelChoiceField = DioeModelChoiceField
 
 # Vorlage zur erstellung von Formularen anhand des Models
 def GetModelForm(amodel, *args, **kwargs):
