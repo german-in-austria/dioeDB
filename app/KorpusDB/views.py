9d58540b6aa68739b66c666785132658724ec54d
hcb79 <hcb@hcb.de>
2017-05-16 13:03:02 +0200

    KorpusDB - Eingabemaske hinzugefügt.

--- app/KorpusDB/views.py
+++ app/KorpusDB/views.py
@@ -7,6 +7,7 @@ import datetime
 import json
 from .models import sys_presettags
 import KorpusDB.models as dbmodels
+import PersonenDB.models as PersonenDB
 
 def aufgabensets(request):
 	info = ''
@@ -83,8 +84,131 @@ def tagsedit(request):
 	return formularView(app_name,tabelle_name,permName,primaerId,aktueberschrift,asurl,aufgabenform,request,info,error)
 
 def maske(request,ipk=0,apk=0):
-	pass
-
+	# Ist der User Angemeldet?
+	if not request.user.is_authenticated():
+		return redirect('dissdb_login')
+	if not request.user.has_perm('KorpusDB.antworten_maskEdit'):
+		return redirect('Startseite:start')
+	aFormular = 'korpusdbmaske/start_formular.html'
+	test = ''
+	error = ''
+	apk=int(apk)
+	ipk=int(ipk)
+	if apk>0 and ipk>0:
+		if 'save' in request.POST:
+			if request.POST.get('save') == 'ErhInfAufgaben':
+				saveErhInfAufgaben = dbmodels.tbl_erhinfaufgaben.objects.get(pk=int(request.POST.get('pk')))
+				saveErhInfAufgaben.start_Aufgabe = datetime.timedelta(microseconds=int(float(request.POST.get('start_Aufgabe') if request.POST.get('start_Aufgabe') else 0)*1000000))
+				saveErhInfAufgaben.stop_Aufgabe = datetime.timedelta(microseconds=int(float(request.POST.get('stop_Aufgabe') if request.POST.get('stop_Aufgabe') else 0)*1000000))
+				saveErhInfAufgaben.save()
+				aFormular = 'korpusdbmaske/audio_formular.html'
+			elif request.POST.get('save') == 'Aufgaben':
+				for aAntwort in json.loads(request.POST.get('aufgaben')):
+					if 'delit' in aAntwort:		# Löschen
+						aDelAntwort = dbmodels.tbl_antworten.objects.get(pk=aAntwort['id_Antwort'])
+						test+=str(aDelAntwort)+' Löschen!<br>'
+						if aDelAntwort.ist_Satz:
+							aDelAntwort.ist_Satz.delete()
+							test+='Satz gelöscht<br>'
+						aDelAntwort.delete()
+						test+='<hr>'
+					else:						# Speichern/Erstellen
+						if aAntwort['Kommentar'] or aAntwort['ist_Satz_Standardorth'] or aAntwort['ist_bfl'] or aAntwort['bfl_durch_S'] or aAntwort['ist_Satz_Transkript'] or aAntwort['start_Antwort'] or aAntwort['stop_Antwort'] or aAntwort['tags']:
+							if int(aAntwort['id_Antwort']) > 0:		# Speichern
+								aSaveAntwort = dbmodels.tbl_antworten.objects.get(pk=aAntwort['id_Antwort'])
+								sTyp = ' gespeichert!<br>'
+							else:									# Erstellen
+								aSaveAntwort = dbmodels.tbl_antworten()
+								sTyp = ' erstellt!<br>'
+							aSaveAntwort.ist_gewaehlt = False
+							aSaveAntwort.ist_nat = False
+							aSaveAntwort.von_Inf = PersonenDB.tbl_informanten.objects.get(pk=int(aAntwort['von_Inf']))
+							aSaveAntwort.zu_Aufgabe = dbmodels.tbl_aufgaben.objects.get(pk=int(aAntwort['zu_Aufgabe']))
+							aSaveAntwort.Reihung = int(aAntwort['reihung'])
+							aSaveAntwort.ist_bfl = aAntwort['ist_bfl']
+							aSaveAntwort.bfl_durch_S = aAntwort['bfl_durch_S']
+							aSaveAntwort.start_Antwort = datetime.timedelta(microseconds=int(float(aAntwort['start_Antwort'] if aAntwort['start_Antwort'] else 0)*1000000))
+							aSaveAntwort.stop_Antwort = datetime.timedelta(microseconds=int(float(aAntwort['stop_Antwort'] if aAntwort['stop_Antwort'] else 0)*1000000))
+							aSaveAntwort.Kommentar = aAntwort['Kommentar']
+							if int(aAntwort['ist_Satz_pk']) > 0:	# Satz bearbeiten
+								asSatz = dbmodels.tbl_saetze.objects.get(pk=aAntwort['ist_Satz_pk'])
+								ssTyp = ' gespeichert!<br>'
+							else:									# Satz erstellen
+								asSatz = dbmodels.tbl_saetze()
+								ssTyp = ' erstellt!<br>'
+							asSatz.Transkript = aAntwort['ist_Satz_Transkript']
+							asSatz.Standardorth = aAntwort['ist_Satz_Standardorth']
+							asSatz.save()
+							aSaveAntwort.ist_Satz = asSatz
+							test+= 'Satz "'+str(aSaveAntwort.ist_Satz)+'" (PK: '+str(aSaveAntwort.ist_Satz.pk)+')'+ssTyp
+							aSaveAntwort.save()
+							for asTag in aAntwort['tags']:
+								if int(asTag['id_tag'])==0 or int(asTag['id_TagEbene'])==0:
+									if int(asTag['pk']) > 0:
+										aDelAntwortenTag = dbmodels.tbl_antwortentags.objects.get(pk=int(asTag['pk']))
+										test+= 'AntwortenTag "'+str(aDelAntwortenTag)+'" (PK: '+str(aDelAntwortenTag.pk)+') gelöscht!<br>'
+										aDelAntwortenTag.delete()
+								else:
+									if int(asTag['pk']) > 0:		# Tag bearbeiten
+										asAntwortenTag = dbmodels.tbl_antwortentags.objects.get(pk=int(asTag['pk']))
+										stTyp = ' gespeichert!<br>'
+									else:							# Tag erstellen
+										asAntwortenTag = dbmodels.tbl_antwortentags()
+										stTyp = ' erstellt!<br>'
+									asAntwortenTag.id_Antwort = aSaveAntwort
+									asAntwortenTag.id_Tag =  dbmodels.tbl_tags.objects.get(pk=int(asTag['id_tag']))
+									asAntwortenTag.id_TagEbene =  dbmodels.tbl_tagebene.objects.get(pk=int(asTag['id_TagEbene']))
+									asAntwortenTag.Reihung =  int(asTag['reihung'])
+									asAntwortenTag.save()
+									test+= 'AntwortenTag "'+str(asAntwortenTag)+'" (PK: '+str(asAntwortenTag.pk)+')'+stTyp
+							test+= 'Antwort "'+str(aSaveAntwort)+'" (PK: '+str(aSaveAntwort.pk)+')'+sTyp+'<hr>'
+				aFormular = 'korpusdbmaske/antworten_formular.html'
+		Informant = PersonenDB.tbl_informanten.objects.get(pk=ipk)
+		Aufgabe = dbmodels.tbl_aufgaben.objects.get(pk=apk)
+		eAntwort = dbmodels.tbl_antworten()
+		eAntwort.von_Inf = Informant
+		eAntwort.zu_Aufgabe = Aufgabe
+		TagEbenen = dbmodels.tbl_tagebene.objects.all()
+		TagsList = getTagList(dbmodels.tbl_tags,None)
+		Antworten = []
+		for val in dbmodels.tbl_antworten.objects.filter(von_Inf=ipk,zu_Aufgabe=apk).order_by('Reihung'):
+			ptags=dbmodels.tbl_antwortentags.objects.filter(primaer=True,id_Antwort=val.pk).order_by('Reihung')
+			stags=dbmodels.tbl_antwortentags.objects.filter(primaer=False,id_Antwort=val.pk).order_by('Reihung')
+			xtags = []
+			for xval in dbmodels.tbl_antwortentags.objects.filter(id_Antwort=val.pk).values('id_TagEbene').annotate(total=Count('id_TagEbene')).order_by('id_TagEbene'):
+				xtags.append({'ebene':dbmodels.tbl_tagebene.objects.filter(pk=xval['id_TagEbene']), 'tags':getTagFamilie(dbmodels.tbl_antwortentags.objects.filter(id_Antwort=val.pk, id_TagEbene=xval['id_TagEbene']).order_by('Reihung'))})
+			Antworten.append({'model':val, 'ptags':ptags, 'stags':stags, 'xtags':xtags})
+		Antworten.append(eAntwort)
+		ErhInfAufgaben = dbmodels.tbl_erhinfaufgaben.objects.filter(id_Aufgabe=apk,id_InfErh__ID_Inf__pk=ipk)
+		aPresetTags = []
+		for val in PresetTags.objects.all():
+			aPresetTags.append({'model':val,'tagfamilie':getTagFamiliePT(val.id_Tags.all())})
+		return render_to_response(aFormular,
+			RequestContext(request, {'Informant':Informant,'Aufgabe':Aufgabe,'Antworten':Antworten, 'TagEbenen':TagEbenen ,'TagsList':TagsList,'ErhInfAufgaben':ErhInfAufgaben,'PresetTags':aPresetTags,'test':test,'error':error}),)
+	InformantenCount=PersonenDB.tbl_informanten.objects.all().count()
+	aAufgabenset = 0 ; Aufgabensets = [{'model':val,'Acount':dbmodels.tbl_aufgaben.objects.filter(von_ASet = val.pk).count()} for val in dbmodels.tbl_aufgabensets.objects.all()]
+	aAufgabe = 0		; Aufgaben = None
+	Informanten = None
+	if 'aaufgabenset' in request.POST:
+		aAufgabenset = int(request.POST.get('aaufgabenset'))
+		if 'infantreset' in request.POST:		# InformantenAntwortenUpdate
+			aAufgabe = int(request.POST.get('aaufgabe'))
+			aResponse = HttpResponse(str({str(val.pk):str(dbmodels.tbl_antworten.objects.filter(von_Inf=val,zu_Aufgabe=aAufgabe).count()) for val in PersonenDB.tbl_informanten.objects.all()}).replace("'",'"'))
+			aResponse['Content-Type'] = 'text/text'
+			return aResponse
+		if aAufgabenset == int(request.POST.get('laufgabenset')):
+			aAufgabe = int(request.POST.get('aaufgabe'))
+			Informanten = [{'model':val,'count':dbmodels.tbl_antworten.objects.filter(von_Inf=val,zu_Aufgabe=aAufgabe).count} for val in PersonenDB.tbl_informanten.objects.all()]
+		Aufgaben = []
+		for val in dbmodels.tbl_aufgaben.objects.filter(von_ASet=aAufgabenset):
+			try:
+				aproz = (100/InformantenCount*dbmodels.tbl_antworten.objects.filter(zu_Aufgabe=val.pk).values('zu_Aufgabe').annotate(total=Count('von_Inf'))[0]['total'])
+			except:
+				aproz = 0
+			Aufgaben.append({'model':val, 'aProz': aproz})
+	# Ausgabe der Seite
+	return render_to_response('korpusdbmaske/start.html',
+		RequestContext(request, {'aAufgabenset':aAufgabenset,'Aufgabensets':Aufgabensets,'aAufgabe':aAufgabe,'Aufgaben':Aufgaben,'Informanten':Informanten,'test':test}),)
 
 
 
@@ -121,6 +245,7 @@ def getTagFamilie(Tags):
 		afam.append(value.id_Tag)
 		aGen+=1
 	return oTags
+
 # getTagFamilie für PresetTags
 def getTagFamiliePT(Tags):
 	afam = []
