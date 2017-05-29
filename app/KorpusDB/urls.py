93c45c65afe4c68430726f1dcabfb73ba118d4c4
hcb79 <hcb@hcb.de>
2017-05-29 15:48:38 +0200

    DB - Auswertungssystem hinzugefügt. (Unfertig/Testversion)

--- app/KorpusDB/urls.py
+++ app/KorpusDB/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^presettagsedit/{0,1}$', views.presettagsedit, name='presettagsedit'),
 	url(r'^maske/(?P<ipk>[A-ZÄÖÜa-zäöü0-9_]+)/(?P<apk>[A-ZÄÖÜa-zäöü0-9_]+)/{0,1}$', views.maske, name='maske'),
 	url(r'^inferhebung/{0,1}$', views.inferhebung, name='inferhebung'),
+	url(r'^auswertung/{0,1}$', views.auswertung, name='auswertung'),
 ]
