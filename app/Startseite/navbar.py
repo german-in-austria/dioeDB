a36bb811527d72d95aeb6c3b62b7e628e685ae76
hcb79 <hcb@hcb.de>
2017-06-17 12:24:18 +0200

    System - Passwort ändern hinzugefügt.

--- app/Startseite/navbar.py
+++ app/Startseite/navbar.py
@@ -6,7 +6,8 @@ def navbar(request):
         anav.append({'sort':0,'titel':'Startseite','url':reverse('Startseite:start'),'class':''})
         asub = []
         if request.user.is_superuser:
-            asub.append({'sort':98,'titel':'Admin','url':reverse('admin:index'),'target':'_blank'})
+            asub.append({'sort':50,'titel':'Admin','url':reverse('admin:index'),'target':'_blank'})
+        asub.append({'sort':60,'titel':'Passwort ändern','url':reverse('password_change')})
         asub.append({'sort':99,'titel':'Abmelden','url':reverse('dioedb_logout')})
         anav.append({'sort':99,'titel':request.user.username,'url':'#','aclass':'text-uppercase','sub':asub})
     return anav
--- app/dioeDB/urls.py
+++ app/dioeDB/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     url(r'^personendb/', include('PersonenDB.urls', namespace='PersonenDB')),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'main/login.html'}, name='dioedb_login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': reverse_lazy('Startseite:start')}, name='dioedb_logout'),
+    url(r'^change-password/$', 'django.contrib.auth.views.password_change', {'template_name': 'main/change-password.html'}, name='password_change'),
+    url(r'^change-password/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'main/change-password-done.html'}, name='password_change_done'),
    	url(r'^sysstatus/{0,1}$', Startseite_views.sysStatusView, name='sysstatus'),
     url(r'^private-media/', include(private_storage.urls)),
 ]
