1f0c102c60627fecd93521c034a40e658eae83d4
hcb79 <hcb@hcb.de>
2017-08-02 14:14:21 +0200

    KorpusDB - CSV Import - Datei Informationen und CSV Spalten anzeigen.

--- app/KorpusDB/static/korpusdb/js/csv.js
+++ app/KorpusDB/static/korpusdb/js/csv.js
@@ -1,10 +1,23 @@
 (function($){jQuery(document).ready(function($){
 	$(document).on('change','#csv-select-tree',function(e){
-		$.post('/korpusdb/csv/',{ csrfmiddlewaretoken: csrf , csvSelDir:$(this).val(), csvSelDirCol:1}, function(d) {
+		$.post('/korpusdb/csv/',{ csrfmiddlewaretoken: csrf, csvSelDir:$(this).val(), csvSelDirCol:1}, function(d) {
 			if(d.substring(0, 6)=='Fehler') {
 				alert(d)
 			} else {
 				$('.csvselectcol').html(d)
+			}
+		}).fail(function(d) {
+			alert( "error: " + d )
+			console.log(d)
+		})
+	});
+	$(document).on('change','#csv-select-file',function(e){
+		$.post('/korpusdb/csv/',{ csrfmiddlewaretoken: csrf, csvSelFile:$(this).val(), csvSelDir:$('#csv-select-tree').val()}, function(d) {
+			if(d.substring(0, 6)=='Fehler') {
+				alert(d)
+			} else {
+				$('.csvselectcol').html($(d).find('.csvselectcol').html())
+				$('.mcon').html($(d).find('.mcon').html())
 			}
 		}).fail(function(d) {
 			alert( "error: " + d )
--- app/KorpusDB/view_csv.py
+++ app/KorpusDB/view_csv.py
@@ -4,7 +4,8 @@ from DB.funktionenDB import httpOutput
 from .view_dateien import getPermission, scanFiles, scanDir, removeLeftSlash, tree2select
 from django.conf import settings
 import datetime
-import os
+import collections
+import os, csv
 
 def view_csv(request):
 	info = ''
@@ -29,16 +30,33 @@ def view_csv(request):
 		if aFile['type'] == 'csv':
 			files.append({'title':aFile['name'],'value':aFile['name']})
 	csvSelFile = None
+	csvSelFileData = collections.OrderedDict()
+	csvRows = []
 	if 'csvSelFile' in request.POST:
 		csvSelFile = request.POST.get('csvSelFile')
 		csvSelFileABS = os.path.normpath(os.path.join(csvSelDirABS,removeLeftSlash(csvSelFile)))
 		if not os.path.isfile(csvSelFileABS):
 			return httpOutput('Fehler: Ausgewähltes Datei existiert nicht!')
+		csvSelFileData['Name:'] = csvSelFile
+		if os.stat_float_times():
+			lmod = datetime.datetime.utcfromtimestamp(os.path.getmtime(csvSelFileABS))
+		else:
+			lmod = os.path.getmtime(csvSelFileABS)
+		csvSelFileData['Datum'] = lmod.strftime("%d.%m.%Y - %H:%M")+" Uhr"
+		csvSelFileData['Größe'] = os.path.getsize(csvSelFileABS)
+		with open(csvSelFileABS, encoding='utf-8') as csvfile:
+			reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+			aLine = 0
+			for row in reader:
+				aLine+=1
+				csvRows.append(row)
+		csvSelFileData['Zeilen'] = aLine-1
+		# CSV analysieren
 
 
 	if 'csvSelDirCol' in request.POST:
 		return render_to_response('korpusdb/csv_selectcol.html',
-			RequestContext(request, {'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile}),)
+			RequestContext(request, {'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData}),)
 
 	return render_to_response('korpusdb/csv_start.html',
-		RequestContext(request, {'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'info':info,'error':error}),)
+		RequestContext(request, {'csvRows':csvRows,'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData,'info':info,'error':error}),)
