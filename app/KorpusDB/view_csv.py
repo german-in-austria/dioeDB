51fe5ec5637333a2e09a1ae8cb4259ac3490914f
hcb79 <hcb@hcb.de>
2017-08-03 13:03:54 +0200

    KorpusDB - CSV Import - Zellenauswertung vorbereitet. Fehleranzeige für CSV, Spalten und Zellen hinzugefügt.

--- app/KorpusDB/view_csv.py
+++ app/KorpusDB/view_csv.py
@@ -32,12 +32,13 @@ def view_csv(request):
 	csvSelFile = None
 	csvSelFileData = collections.OrderedDict()
 	csvRows = []
+	csvData = {}
 	if 'csvSelFile' in request.POST:
 		csvSelFile = request.POST.get('csvSelFile')
 		csvSelFileABS = os.path.normpath(os.path.join(csvSelDirABS,removeLeftSlash(csvSelFile)))
 		if not os.path.isfile(csvSelFileABS):
 			return httpOutput('Fehler: Ausgewähltes Datei existiert nicht!')
-		csvSelFileData['Name:'] = csvSelFile
+		csvSelFileData['Name'] = csvSelFile
 		if os.stat_float_times():
 			lmod = datetime.datetime.utcfromtimestamp(os.path.getmtime(csvSelFileABS))
 		else:
@@ -52,11 +53,50 @@ def view_csv(request):
 				csvRows.append(row)
 		csvSelFileData['Zeilen'] = aLine-1
 		# CSV analysieren
-
-
+		aRow = 0
+		rowErrors = 0
+		csvData = {'colDef':[],'colUse':['ID_Aufgabe'],'rows':[],'dispRows':[],'colError':{}}
+		csvCountImport = {'ID_Aufgabe':0}
+		from .models import tbl_aufgaben
+		for csvRow in csvRows:
+			aRow+=1
+			if aRow == 1:
+				for csvCell in csvRow:
+					csvData['colDef'].append(csvCell)
+			else:
+				aCell = 0
+				aRowData = {}
+				for csvCell in csvRow:
+					rowHasError = 0
+					aRowData[csvData['colDef'][aCell]] = {'value': csvCell}
+					# Feld auf Fehler überprüfen!
+					if csvData['colDef'][aCell] == 'ID_Aufgabe':
+						try:
+							aTblGet = tbl_aufgaben.objects.get(pk=int(csvCell))
+							aRowData[csvData['colDef'][aCell]]['conTable'] = aTblGet
+							csvCountImport['ID_Aufgabe']+=1
+						except:
+							if not csvData['colDef'][aCell] in csvData['colError']:
+								csvData['colError'][csvData['colDef'][aCell]] = {}
+							aRowData[csvData['colDef'][aCell]]['error'] = 'Aufgabe ist in der Datenbank nicht vorhanden!'
+							csvData['colError'][csvData['colDef'][aCell]]['tbl_aufgaben_nx'] = aRowData[csvData['colDef'][aCell]]['error']
+							rowHasError = 1
+					aCell+=1
+				csvData['rows'].append(aRowData)
+				if aRow < 12:
+					csvData['dispRows'].append(aRowData)
+				elif rowHasError == 1:
+					rowErrors+=1
+					if rowErrors < 12:
+						csvData['dispRows'].append(aRowData)
+		if csvCountImport['ID_Aufgabe']<csvSelFileData['Zeilen']:
+			if csvCountImport['ID_Aufgabe'] == 0:
+				error+= 'Spalte "<b>ID_Aufgabe</b>" fehlt!<br>'
+			else:
+				error+= 'Spalte "<b>ID_Aufgabe</b>" unvollständig! ('+str(csvCountImport['ID_Aufgabe'])+'/'+str(csvSelFileData['Zeilen'])+')<br>'
 	if 'csvSelDirCol' in request.POST:
 		return render_to_response('korpusdb/csv_selectcol.html',
 			RequestContext(request, {'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData}),)
 
 	return render_to_response('korpusdb/csv_start.html',
-		RequestContext(request, {'csvRows':csvRows,'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData,'info':info,'error':error}),)
+		RequestContext(request, {'csvData':csvData,'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData,'info':info,'error':error}),)
