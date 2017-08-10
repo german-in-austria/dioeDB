823cd61ca7225db7c1c411361e2be80bce9f35f0
hcb79 <hcb@hcb.de>
2017-08-10 13:09:49 +0200

    KorpusDB - CSV Import - Code optimiert/übersichtlicher.

--- app/KorpusDB/view_csv.py
+++ app/KorpusDB/view_csv.py
@@ -10,6 +10,7 @@ import os, csv
 def view_csv(request):
 	info = ''
 	error = ''
+	# Test ...
 	mDir = getattr(settings, 'PRIVATE_STORAGE_ROOT', None)
 	if not mDir:
 		return HttpResponseServerError('PRIVATE_STORAGE_ROOT wurde nicht gesetzt!')
@@ -31,8 +32,8 @@ def view_csv(request):
 			files.append({'title':aFile['name'],'value':aFile['name']})
 	csvSelFile = None
 	csvSelFileData = collections.OrderedDict()
-	csvRows = []
 	csvData = {}
+	# Datei auswerten
 	if 'csvSelFile' in request.POST:
 		csvSelFile = request.POST.get('csvSelFile')
 		csvSelFileABS = os.path.normpath(os.path.join(csvSelDirABS,removeLeftSlash(csvSelFile)))
@@ -45,50 +46,31 @@ def view_csv(request):
 			lmod = os.path.getmtime(csvSelFileABS)
 		csvSelFileData['Datum'] = lmod.strftime("%d.%m.%Y - %H:%M")+" Uhr"
 		csvSelFileData['Größe'] = os.path.getsize(csvSelFileABS)
-		with open(csvSelFileABS, encoding='utf-8') as csvfile:
-			reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-			aLine = 0
-			for row in reader:
-				aLine+=1
-				csvRows.append(row)
-		csvSelFileData['Zeilen'] = aLine-1
+		csvData = getCsvData(getCsvFile(csvSelFileABS))
+		csvSelFileData['Zeilen'] = csvData['rowCount']
+		csvSelFileData['Spalten'] = csvData['colCount']
+		csvData['colUse'] = ['ID_Aufgabe']
+
 		# CSV analysieren
-		aRow = 0
-		rowErrors = 0
-		csvData = {'colDef':[],'colUse':['ID_Aufgabe'],'rows':[],'dispRows':[],'colError':{}}
-		csvCountImport = {'ID_Aufgabe':0}
 		from .models import tbl_aufgaben
-		for csvRow in csvRows:
-			aRow+=1
-			if aRow == 1:
-				for csvCell in csvRow:
-					csvData['colDef'].append(csvCell)
-			else:
-				aCell = 0
-				aRowData = {}
-				for csvCell in csvRow:
-					rowHasError = 0
-					aRowData[csvData['colDef'][aCell]] = {'value': csvCell}
-					# Feld auf Fehler überprüfen!
-					if csvData['colDef'][aCell] == 'ID_Aufgabe':
-						try:
-							aTblGet = tbl_aufgaben.objects.get(pk=int(csvCell))
-							aRowData[csvData['colDef'][aCell]]['conTable'] = aTblGet
-							csvCountImport['ID_Aufgabe']+=1
-						except:
-							if not csvData['colDef'][aCell] in csvData['colError']:
-								csvData['colError'][csvData['colDef'][aCell]] = {}
-							aRowData[csvData['colDef'][aCell]]['error'] = 'Aufgabe ist in der Datenbank nicht vorhanden!'
-							csvData['colError'][csvData['colDef'][aCell]]['tbl_aufgaben_nx'] = aRowData[csvData['colDef'][aCell]]['error']
-							rowHasError = 1
-					aCell+=1
-				csvData['rows'].append(aRowData)
-				if aRow < 12:
-					csvData['dispRows'].append(aRowData)
-				elif rowHasError == 1:
-					rowErrors+=1
-					if rowErrors < 12:
-						csvData['dispRows'].append(aRowData)
+		csvCountImport = {'ID_Aufgabe':0}
+		for csvRow in csvData['rows']:
+			rowHasError = 0
+			# Fehler Prüfung für 'ID_Aufgabe'
+			if 'ID_Aufgabe' in csvData['colDef']:
+				try:
+					aTblGet = tbl_aufgaben.objects.get(pk=int(csvRow['cols']['ID_Aufgabe']['value']))
+					csvRow['cols']['ID_Aufgabe']['conTable'] = aTblGet
+					csvCountImport['ID_Aufgabe']+=1
+				except:
+					if not 'ID_Aufgabe' in csvData['colError']:
+						csvData['colError']['ID_Aufgabe'] = {}
+					csvRow['cols']['ID_Aufgabe']['error'] = 'Aufgabe ist in der Datenbank nicht vorhanden!'
+					csvData['colError']['ID_Aufgabe']['tbl_aufgaben_nx'] = csvRow['cols']['ID_Aufgabe']['error']
+					rowHasError = 1
+			# Vorschau Daten
+			if len(csvData['dispRows']) < 10 or (rowHasError == 1 and len(csvData['dispRows']) < 20):
+				csvData['dispRows'].append(csvRow)
 		if csvCountImport['ID_Aufgabe']<csvSelFileData['Zeilen']:
 			if csvCountImport['ID_Aufgabe'] == 0:
 				error+= 'Spalte "<b>ID_Aufgabe</b>" fehlt!<br>'
@@ -100,3 +82,28 @@ def view_csv(request):
 
 	return render_to_response('korpusdb/csv_start.html',
 		RequestContext(request, {'csvData':csvData,'tree':tree,'csvSelDir':csvSelDir,'files':files,'csvSelFile':csvSelFile,'csvSelFileData':csvSelFileData,'info':info,'error':error}),)
+
+def getCsvData(rows):
+	csvData = {'colDef':[],'rows':[],'dispRows':[],'colError':{},'rowCount':0,'colCount':0}
+	for csvRow in rows:
+		if csvData['rowCount'] == 0:
+			for csvCell in csvRow:
+				csvData['colCount']+=1
+				csvData['colDef'].append(csvCell)
+		else:
+			aCell = 0
+			aRowData = {}
+			for csvCell in csvRow:
+				aRowData[csvData['colDef'][aCell]] = {'value': csvCell}
+				aCell+=1
+			csvData['rows'].append({'nr': csvData['rowCount'],'cols':aRowData})
+		csvData['rowCount']+=1
+	return csvData
+
+def getCsvFile(file):
+	csvRows = []
+	with open(file, encoding='utf-8') as csvfile:
+		reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+		for row in reader:
+			csvRows.append(row)
+	return csvRows
