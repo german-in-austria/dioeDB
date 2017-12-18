5830905d83d8232068e295a0f4dffb840c84e25a
hcb79 <hcb@hcb.de>
2017-12-18 12:39:40 +0100

    DB - Diagramm - Code optimierung.

--- app/DB/view_diagramm.py
+++ app/DB/view_diagramm.py
@@ -1,30 +1,25 @@
+"""Anzeige eines Diagramms der Models."""
 from django.shortcuts import render_to_response
-from django.http import HttpResponseNotFound
 from django.template import RequestContext
 from django.apps import apps
-import collections
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
-from DB.forms import GetModelForm
 from DB.funktionenDB import httpOutput
 from django.conf import settings
 import json
-from django.db import connection
-import pprint
 from .models import sys_diagramm_tabellenpositionen
 
+
 def view_diagramm(request):
+	"""Standard Anzeige für Model Diagramme."""
 	info = ''
 	error = ''
 	# Modelposition speichern
 	if 'speichere' in request.POST:
-		if request.POST.get('speichere')=='positionen':
+		if request.POST.get('speichere') == 'positionen':
 			positionen = json.loads(request.POST.get('positionen'))
 			for position in positionen:
-				if request.user.has_perm(position['app']+'.edit'):
+				if request.user.has_perm(position['app'] + '.edit'):
 					try:
-						amodel = sys_diagramm_tabellenpositionen.objects.get(zu_app=position['app'],zu_model=position['model'])
+						amodel = sys_diagramm_tabellenpositionen.objects.get(zu_app=position['app'], zu_model=position['model'])
 					except:
 						amodel = sys_diagramm_tabellenpositionen()
 						amodel.zu_app = position['app']
@@ -37,44 +32,48 @@ def view_diagramm(request):
 	tabellen = []
 	applist = settings.DIOEDB_APPLIST
 	for aapp in applist:
-		if request.user.has_perm(aapp+'.edit'):
+		if request.user.has_perm(aapp + '.edit'):
 			for model in apps.get_app_config(aapp).models.items():
-				if str(model[0])[:4]!='sys_':
+				if str(model[0])[:4] != 'sys_':
 					amodel = apps.get_model(aapp, model[0])
 					aFields = []
 					xt = 0
 					yt = 0
 					try:
-						asdtp = sys_diagramm_tabellenpositionen.objects.get(zu_app=aapp,zu_model=str(model[0]))
+						asdtp = sys_diagramm_tabellenpositionen.objects.get(zu_app=aapp, zu_model=str(model[0]))
 						xt = asdtp.xt
 						yt = asdtp.yt
 					except:
 						pass
 					for f in amodel._meta.get_fields():
-						if not f.auto_created or amodel._meta.pk.name==f.name:
-							aField = {'field_name':f.name,
-									  'verbose_name':f._verbose_name,
-									  'internal_type':f.get_internal_type(),
-									  'unique':f.unique,
-									  'blank':f.blank,
-									  'null':f.null,
-									 }
-							if amodel._meta.pk.name==f.name:
+						if not f.auto_created or amodel._meta.pk.name == f.name:
+							aField = {
+								'field_name': f.name,
+								'verbose_name': f._verbose_name,
+								'internal_type': f.get_internal_type(),
+								'unique': f.unique,
+								'blank': f.blank,
+								'null': f.null,
+							}
+							if amodel._meta.pk.name == f.name:
 								aField['pk'] = True
 							if f.is_relation:
 								aField['related_db_table'] = f.related_model._meta.db_table
 							aFields.append(aField)
-					tabellen.append({'model':model[0],
-								    'app':aapp,
-								    'verbose_name':amodel._meta.verbose_name,
-								    'verbose_name_plural':amodel._meta.verbose_name_plural,
-								    'count':amodel.objects.count(),
-								    'db_table':amodel._meta.db_table,
-								    'get_fields':aFields,
-									'xt':xt,
-									'yt':yt,
-								   })
+					tabellen.append({
+						'model': model[0],
+						'app': aapp,
+						'verbose_name': amodel._meta.verbose_name,
+						'verbose_name_plural': amodel._meta.verbose_name_plural,
+						'count': amodel.objects.count(),
+						'db_table': amodel._meta.db_table,
+						'get_fields': aFields,
+						'xt': xt,
+						'yt': yt,
+					})
 	tabellen = json.dumps(tabellen)
 	# Ausgabe der Seite
-	return render_to_response('DB/diagramm.html',
-		RequestContext(request, {'tabellen':tabellen,'error':error,'info':info}),)
+	return render_to_response(
+		'DB/diagramm.html',
+		RequestContext(request, {'tabellen': tabellen, 'error': error, 'info': info}),
+	)
