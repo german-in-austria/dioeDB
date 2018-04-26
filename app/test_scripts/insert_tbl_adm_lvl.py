7d941d65562eeb3cf35ada361e3e89e960e63979
van15h <ivanvarabyeu@gmail.com>
2018-04-26 12:37:53 +0200

    test script to create tbl_adm_lvl data

---
+++ app/test_scripts/insert_tbl_adm_lvl.py
@@ -0,0 +1,28 @@
+# python3 manage.py shell < test_scripts/insert_tbl_adm_lvl.py
+
+from mioeDB.models import tbl_adm_lvl
+tbl_adm_lvl.objects.all()
+#q = tbl_adm_lvl(name="")
+#q.save()
+# q = tbl_adm_lvl(name="")
+# q.save()
+# q = tbl_adm_lvl(name="")
+# q.save()
+q = tbl_adm_lvl(pk=4,name="Bundesland")
+q.save()
+# q = tbl_adm_lvl(name="")
+# q.save()
+q = tbl_adm_lvl(pk=6, name="Politischer Bezirk")
+q.save()
+# q = tbl_adm_lvl(name="")
+# q.save()
+q = tbl_adm_lvl(pk=8, name="Gemeinde")
+q.save()
+q = tbl_adm_lvl(pk=9, name="Ort")
+q.save()
+q = tbl_adm_lvl(pk=10, name="Stadtteile")
+q.save()
+tbl_adm_lvl.objects.all()
+
+#tbl_adm_lvl.objects.all().delete()
+#tbl_adm_lvl.objects.filter(id=4)
