3632c5f6e0b25c2f640ed003ee586fd747015270
van15h <ivanvarabyeu@gmail.com>
2018-05-03 15:19:48 +0200

    new models, eingabemaske v1

--- app/mioeDB/views.py
+++ app/mioeDB/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import redirect
 from DB.funktionenDB import formularView
 from django.core.urlresolvers import reverse
+#from PersonenDB.models import tbl_orte
 
 # Create your views here.
 def mioe(request):
@@ -10,15 +11,10 @@ def mioe(request):
 	error = ''
 	if not request.user.is_authenticated():
 		return redirect('dioedb_login')
-	# to get model
-	app_name = 'mioeDB'
-	tabelle_name = 'tbl_adm_lvl'
-
+	app_name = 'PersonenDB'
+	tabelle_name = 'tbl_orte'
 	permName = 'personen'
-	#TODO was ist das?
 	primaerId = 'adm_lvl'
-
-	# only string
 	aktueberschrift = 'Admin levels'
 	asurl = '/mioedb/orte/'
 	if not request.user.has_perm(app_name + '.' + permName + '_maskView'):
@@ -26,13 +22,23 @@ def mioe(request):
 
 	aufgabenform = [
 		{
-			'titel': 'Adm lvl',
-			'titel_plural': 'Adm lvls',
-			'app': 'mioeDB',
-			'tabelle': 'tbl_adm_lvl',
-			'id': 'tbl_adm_lvl',
+			'titel': 'Ort',
+			'titel_plural': 'Ort',
+			'app': 'PersonenDB',
+			'tabelle': 'tbl_orte',
+			'id': 'tbl_orte',
 			'optionen': ['einzeln', 'elementFrameless'],
-			'felder':['+id', 'name']
+
+			'felder':['+id', 'ort_namekurz', 'adm_lvl', 'lat', 'gid', 'lon', 'histor' ],
+
+			'feldoptionen':{
+				'ort_namekurz': {'label_col': 2, 'input_col': 4},
+				'adm_lvl': {'label_col': 2, 'input_col': 4, 'nl': True},
+				'lat': {'label_col': 2, 'input_col': 4},
+				'gid': {'label_col': 2, 'input_col': 4, 'nl': True},
+				'lon': {'label_col': 2, 'input_col': 4},
+				'histor': {'label_col': 2, 'input_col': 4, 'nl': True},
+			}
 		}
 	]
 	return formularView(app_name, tabelle_name, permName, primaerId, aktueberschrift, asurl, aufgabenform, request, info, error)
--- app/test_scripts/insert_tbl_adm_lvl.py
+++ app/test_scripts/insert_tbl_adm_lvl.py
@@ -1,21 +1,12 @@
 # python3 manage.py shell < test_scripts/insert_tbl_adm_lvl.py
 
 from mioeDB.models import tbl_adm_lvl
+tbl_adm_lvl.objects.all().delete()
 tbl_adm_lvl.objects.all()
-#q = tbl_adm_lvl(name="")
-#q.save()
-# q = tbl_adm_lvl(name="")
-# q.save()
-# q = tbl_adm_lvl(name="")
-# q.save()
 q = tbl_adm_lvl(pk=4,name="Bundesland")
 q.save()
-# q = tbl_adm_lvl(name="")
-# q.save()
 q = tbl_adm_lvl(pk=6, name="Politischer Bezirk")
 q.save()
-# q = tbl_adm_lvl(name="")
-# q.save()
 q = tbl_adm_lvl(pk=8, name="Gemeinde")
 q.save()
 q = tbl_adm_lvl(pk=9, name="Ort")
@@ -23,6 +14,8 @@ q.save()
 q = tbl_adm_lvl(pk=10, name="Stadtteile")
 q.save()
 tbl_adm_lvl.objects.all()
+tbl_adm_lvl.objects.filter(id=4)
 
-#tbl_adm_lvl.objects.all().delete()
-#tbl_adm_lvl.objects.filter(id=4)
+from mioeDB.models import tbl_adm_lvl
+
+
