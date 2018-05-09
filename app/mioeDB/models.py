a78e37c3f646437926eb2cd00696a0c272b167ff
van15h <ivanvarabyeu@gmail.com>
2018-05-09 11:50:05 +0200

    added wb tables

--- app/mioeDB/models.py
+++ app/mioeDB/models.py
@@ -150,3 +150,163 @@ class tbl_mioe_personen(models.Model):
     verbose_genus = "f"
     ordering = ('nachname',)
     default_permissions = ()
+
+class tbl_wb(models.Model):
+  num_wb = models.IntegerField(verbose_name="Wenkerbogen Nummer")
+  typ_wb = models.CharField(max_length=1, verbose_name="Wenkerbogen Typ")
+  datierung = models.DateField(
+    blank=True, null=True, verbose_name="Datierung"
+  )
+  id_wenkerort = models.ForeignKey(
+    'tbl_mioe_orte',
+    on_delete=models.CASCADE, verbose_name="Wenkerort"
+  )
+  schulort_original = models.CharField(
+    max_length=255, verbose_name="Schulort (original)",
+  )
+  id_lehrer = models.ForeignKey(
+    'tbl_mioe_personen',
+    blank=True, null=True, on_delete=models.CASCADE, verbose_name="Lehrer"
+  )
+  uebersetzt_von = models.CharField(
+    max_length=255, blank=True, null=True, verbose_name="Uebersetzt von"
+  )
+  uebersetzt_klass = models.CharField(
+    max_length=255, blank=True, null=True, verbose_name="Uebersetzt Klass"
+  )
+  alter_geschl_uebesetzer = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Alter, Geschlecht Uebesetzer"
+  )
+  alter_geschl_lehrer = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Alter, Geschlecht Lehrer"
+  )
+  alter_uebesetzer = models.IntegerField(
+    blank=True, null=True,
+    verbose_name="Alter Uebesetzer"
+  )
+  geburtsdatum_uebersetzer = models.DateField(
+    blank=True, null=True,
+    verbose_name="Geburtsdatum Uebesetzer"
+  )
+  geschlecht_uebersetzer = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Geschlecht Uebesetzer"
+  )
+  informationen_zu = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Informationen zu"
+  )
+  andere_sprache_orig = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Andere Sprache (original)"
+  )
+  andere_sprachen = models.BooleanField(verbose_name="Anderen Sprachen")
+  welche_sprachen = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Welche Sprachen"
+  )
+  sprachen_verhaeltnis = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Sprachen Verhaeltnis"
+  )
+  kommentar_wb = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Kommentar Wenkerbogen"
+  )
+  kommentar_wiss = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Kommentar Wiss."
+  )
+  geprueft = models.BooleanField(verbose_name="Geprueft")
+
+  def __str__(self):
+	  return "{} ({})".format(self.num_wb, self.id_wenkerort.id_ort.ort_namekurz)
+
+  class Meta:
+    db_table = "MioeDB_tbl_wb"
+    verbose_name = "Wenkerbogen"
+    verbose_name_plural = "Wenkerbögen"
+    verbose_genus = "m"
+    ordering = ('num_wb',)
+    default_permissions = ()
+
+class tbl_wb_sprache(models.Model):
+  id_wb = models.ForeignKey(
+    'tbl_wb',
+    on_delete=models.CASCADE, verbose_name="Wenkerbogen"
+  )
+  id_sprache = models.ForeignKey(
+    'tbl_sprache',
+    on_delete=models.CASCADE, verbose_name="Sprache"
+  )
+  anteil = models.FloatField(verbose_name="Anteil")
+
+  def __str__(self):
+    return "{} - {}%".format(self.id_sprache.sprache, self.anteil)
+
+  class Meta:
+    db_table = "MioeDB_tbl_wb_sprache"
+    verbose_name = "Wenkerbogen Sprache"
+    verbose_name_plural = "Wenkerbogen Sprachen"
+    verbose_genus = "f"
+    ordering = ('id_wb',)
+    default_permissions = ()
+
+class tbl_quelle(models.Model):
+  wenkerbogen = models.ForeignKey(
+    'tbl_wb',
+    blank=True, null=True,
+    on_delete=models.CASCADE, verbose_name="Wenkerbogen"
+  )
+  literatur = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Literatur"
+  )
+
+  def __str__(self):
+    if self.wenkerbogen is not None:
+      return "wb: {}".format(self.wenkerbogen.num_wb)
+    else:
+      return "lit: {}".format(self.literatur)
+
+  class Meta:
+    db_table = "MioeDB_tbl_quelle"
+    verbose_name = "Quelle"
+    verbose_name_plural = "Quellen"
+    verbose_genus = "f"
+    ordering = ('id',)
+    default_permissions = ()
+
+class tbl_wb_auch_fuer(models.Model):
+  id_wb = models.ForeignKey(
+    'tbl_wb',
+    on_delete=models.CASCADE, verbose_name="Wenkerbogen")
+  id_wbort = models.ForeignKey(
+    'tbl_mioe_orte',
+    on_delete=models.CASCADE, verbose_name="Wenkerort")
+  id_lehrer = models.ForeignKey(
+    'tbl_mioe_personen',
+    on_delete=models.CASCADE, verbose_name="Lehrer")
+  kommentar_wb = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Kommentar Wenkerbogen"
+  )
+  kommentar_wiss = models.CharField(
+    max_length=255, blank=True, null=True,
+    verbose_name="Kommentar Wiss."
+  )
+
+  def __str__(self):
+    return "{}: {}, {}".format(
+      self.id_wb.num_wb, self.id_wbort.ort_namekurz, self.id_lehrer.nachname,
+    )
+
+  class Meta:
+    db_table = "MioeDB_tbl_wb_auch_fuer"
+    verbose_name = "Wenkerbogen auch fuer"
+    verbose_name_plural = "Wenkerbögen auch fuer"
+    verbose_genus = "m"
+    ordering = ('id_wb',)
+    default_permissions = ()
