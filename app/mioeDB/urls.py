d2b9f24a74187d980e5350a2bc3c8fcac8156812
hcb79 <hcb@hcb.de>
2019-01-14 14:20:39 +0100

    Mioe -> Orte Eingabemaske vorbereitet.

--- app/mioeDB/urls.py
+++ app/mioeDB/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^orte/{0,1}$', views.mioe, name='orte'),
+	url(r'^orte/{0,1}$', views.orte, name='orte'),
 	url(r'^wb/{0,1}$', views.wb, name='wb'),
 	url(r'^vz/{0,1}$', views.vz, name='vz'),
 ]
--- app/mioeDB/views.py
+++ app/mioeDB/views.py
@@ -1,7 +1,6 @@
 """Anzeige für MioeDB."""
 from django.shortcuts import redirect
 from DB.funktionenDB import formularView
-# from django.core.urlresolvers import reverse
 # from mioeDB.models import *
 
 
@@ -140,7 +139,7 @@ def vz(request):
 	return formularView(app_name, tabelle_name, permName, primaerId, aktueberschrift, asurl, aufgabenform, request, info, error)
 
 
-def mioe(request):
+def orte(request):
 	"""Eingabe mioe Orte."""
 	info = ''
 	error = ''
@@ -155,38 +154,25 @@ def mioe(request):
 	if not request.user.has_perm(app_name + '.' + permName + '_maskView'):
 		return redirect('Startseite:start')
 	aufgabenform = []
-	# aufgabenform = [
-	# 				{
-	# 				'titel': 'MiÖ',
-	# 				'titel_plural': 'Ort',
-	# 				'app': 'mioeDB',
-	# 				'tabelle': 'tbl_mioe_orte',
-	# 				'id': 'id_orte',
-	# 				'optionen': ['einzeln', 'elementFrameless'],
-	# 				'felder':['+id', 'id_orte', 'adm_lvl', 'gid', 'histor' ],
-	# 				'feldoptionen':{
-	# 					'id_orte': {'label_col': 3, 'input_col': 7, 'nl': True},
-	# 					'adm_lvl': {'label_col': 3, 'input_col': 4, 'nl': True},
-	# 					'gid': {'label_col': 3, 'input_col': 2, 'nl': True},
-	# 					'histor': {'label_col': 3, 'input_col': 4, 'nl': True},
-	# 				},
-	# 				'sub':[
-	# 					{
-	# 							'titel': 'MiÖ',
-	# 							'titel_plural': 'Administrative Zuordnung',
-	# 							'app': 'mioeDB',
-	# 							'tabelle': 'tbl_adm_zuordnung',
-	# 							'id': 'adm_zuordnung',
-	# 							'optionen': ['einzeln', 'elementFrameless'],
-	# 							'felder':['+id', '|id_ort1=parent:id', 'id_ort2', 'id_quelle' ],
-	# 							'feldoptionen':{
-	# 								'id_ort2': {'label_col': 3, 'input_col': 7,
-	# 									'label': 'Gehört zu', 'nl': True},
-	# 								'id_quelle': {'label_col': 3, 'input_col': 5, 'nl': True},
-	# 							},
-	# 					},
-	# 				],
-	# 				'suboption':['tab']
-	# 			},
-	# ]
+	aufgabenform = [{
+		'titel': 'MiÖ', 'titel_plural': 'Ort', 'app': 'mioeDB', 'tabelle': 'tbl_mioe_orte', 'id': 'id_orte', 'optionen': ['einzeln', 'elementFrameless'],
+		'felder':['+id', 'id_orte', 'adm_lvl', 'gid', 'histor'],
+		'feldoptionen':{
+			'id_orte': {},
+			'adm_lvl': {},
+			'gid': {},
+			'histor': {},
+		},
+		'sub': [
+			{
+				'titel': 'MiÖ', 'titel_plural': 'Administrative Zuordnung', 'app': 'mioeDB', 'tabelle': 'tbl_adm_zuordnung', 'id': 'adm_zuordnung', 'optionen': ['einzeln', 'elementFrameless'],
+				'felder':['+id', '|id_ort1=parent:id', 'id_ort2', 'id_quelle'],
+				'feldoptionen':{
+					'id_ort2': {'label': 'Gehört zu', 'nl': True},
+					'id_quelle': {},
+				},
+			},
+		],
+		'suboption': ['tab']
+	}]
 	return formularView(app_name, tabelle_name, permName, primaerId, aktueberschrift, asurl, aufgabenform, request, info, error)
