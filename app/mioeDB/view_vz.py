f7f76141061219fb6f0f3567925e0305b4946eb2
hcb79 <hcb@hcb.de>
2019-02-11 11:01:51 +0100

    MioeDB -> Vorbereitung Formular.

--- app/mioeDB/view_vz.py
+++ app/mioeDB/view_vz.py
@@ -19,7 +19,18 @@ def view_vz(request):
 			aVzListe.append({'id': aVz.pk, 'title': str(aVz), 'erheb_datum': str(aVz.erheb_datum)})
 		return httpOutput(json.dumps({'success': 'success', 'mioeOrte': aVzListe}))
 	if 'getmask' in request.POST:
-		return httpOutput('todo ...')
+		aVzId = int(request.POST.get('getmask'))
+		if 'save' in request.POST:
+			return httpOutput('todo ...')
+		aVz = mioeDB.tbl_volkszaehlung.objects.get(pk=aVzId)
+		vzDaten = []
+		# vzDaten = [{'model': val} for val in KorpusDB.tbl_antworten.objects.filter(von_Inf=ipk, zu_Aufgabe=apk)]
+		# if len(vzDaten) < 1:
+		# 	vzDaten.append({'model': KorpusDB.tbl_antworten})
+		# vzDaten.append({'model': KorpusDB.tbl_antworten, 'addIt': True})
+		return render_to_response(
+			'mioedbvzmaske/vz_daten_formular.html',
+			RequestContext(request, {'aVz': aVz, 'vzDaten': vzDaten, 'test': test}),)
 	# Ausgabe der Seite
 	return render_to_response(
 		'mioedbvzmaske/start.html',
