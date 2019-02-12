a82dccf88765652ff5457d8c318724cabbfdf2ac
hcb79 <hcb@hcb.de>
2019-02-12 12:00:24 +0100

    MioeDB -> Formular -> Speichern.

--- app/mioeDB/static/mioedbvzmaske/js/formular.js
+++ app/mioeDB/static/mioedbvzmaske/js/formular.js
@@ -7,14 +7,17 @@
 			if (!$('.mcon').hasClass('loading')) {
 				$('.mcon').addClass('loading').html('Lade ...');
 				// Speicherdaten ermitteln
-				let sVzData = [];
-				$.post(aurl, { csrfmiddlewaretoken: csrf, getmask: 1, aMioeOrtId: $('#menue-mioe-ort').val(), aVzId: $('#menue-mioe-vz').val(), sVzData: sVzData }, function (d) {
+				$.post(aurl, { csrfmiddlewaretoken: csrf, getmask: 1, aMioeOrtId: $('#menue-mioe-ort').val(), aVzId: $('#menue-mioe-vz').val() }, function (d) {
 					$('.mcon').removeClass('loading');
 					let aData = JSON.parse(d);
-					if (aData.success === 'success') {
+					if (aData.status === 'success') {
+						$('.mcon').html('');
 						$('.mcon').html(aData.html);
+						setTimeout(vzdAnzahlChanged, 250);
+					} else {
+						alert('Es ist ein Fehler aufgetreten:\n\n' + aData.error);
+						console.log(d);
 					}
-					vzdAnzahlChanged();
 				}).fail(function (d) {
 					var aError = d['responseText'].split('\n', 10);
 					alert('Es ist ein Fehler aufgetreten:\n\n' + aError[0] + '\n' + aError[1] + '\n' + aError[2]);
@@ -27,18 +30,33 @@
 			if (!$('.mcon').hasClass('loading')) {
 				$('.mcon').addClass('loading');
 				$('#antwortensave').addClass('disabled');
-				$.post(aurl, { csrfmiddlewaretoken: csrf, getmask: 1, aMioeOrtId: $('#menue-mioe-ort').val(), aVzId: $('#menue-mioe-vz').val() }, function (d) {
+				let sVzData = [];
+				$('.vzd-line').each(function () {
+					let aVzDataSet = $(this);
+					let sVzDataSet = {};
+					sVzDataSet.datenPk = aVzDataSet.find('.vzd-daten-pk').val();
+					sVzDataSet.artId = aVzDataSet.find('.vzd-art').val();
+					sVzDataSet.artAnzahl = aVzDataSet.find('.vzd-anzahl').val();
+					sVzDataSet.artAbwBez = aVzDataSet.find('.vzd-abw-bez').val();
+					sVzData.push(sVzDataSet);
+				});
+				$.post(aurl, { csrfmiddlewaretoken: csrf, getmask: 1, aMioeOrtId: $('#menue-mioe-ort').val(), aVzId: $('#menue-mioe-vz').val(), sVzData: JSON.stringify(sVzData), save: 1 }, function (d) {
 					$('.mcon').removeClass('loading');
 					let aData = JSON.parse(d);
-					if (aData.success === 'success') {
+					if (aData.status === 'success') {
+						$('.mcon').html('');
 						$('.mcon').html(aData.html);
+						setTimeout(vzdAnzahlChanged, 250);
+					} else {
+						alert('Es ist ein Fehler aufgetreten:\n\n' + aData.error);
+						console.log(d);
 					}
-					vzdAnzahlChanged();
 				}).fail(function (d) {
 					var aError = d['responseText'].split('\n', 10);
 					alert('Es ist ein Fehler aufgetreten:\n\n' + aError[0] + '\n' + aError[1] + '\n' + aError[2]);
 					console.log(d);
 					$('.mcon').removeClass('loading');
+					$('#antwortensave').removeClass('disabled');
 				});
 			}
 		});
--- app/mioeDB/view_vz.py
+++ app/mioeDB/view_vz.py
@@ -11,11 +11,11 @@ def view_vz(request):
 	aUrl = '/mioedb/vz/'
 	aDUrl = 'mioeDB:varietaet'
 	test = ''
+	error = ''
 	if 'getmask' in request.POST:
+		aStatus = 'success'
 		aMioeOrtId = int(request.POST.get('aMioeOrtId'))
 		aVzId = int(request.POST.get('aVzId'))
-		if 'save' in request.POST:
-			print(request.POST.get('sVzData'))
 		vzDaten = []
 		aArtenInVZ = []
 		aVz = False
@@ -26,12 +26,36 @@ def view_vz(request):
 			aMioeOrt = mioeDB.tbl_mioe_orte.objects.get(pk=aMioeOrtId)
 		if aVzId > 0 and aMioeOrtId > 0:
 			if 'save' in request.POST:
-				return httpOutput('todo ...')
+				for aDataSet in json.loads(request.POST.get('sVzData')):
+					aDataSet['datenPk'] = int(aDataSet['datenPk'].strip()) if len(aDataSet['datenPk'].strip()) >= 0 else 0
+					aDataSet['artId'] = int(aDataSet['artId'].strip()) if len(aDataSet['artId'].strip()) > 0 else None
+					aDataSet['artAnzahl'] = int(aDataSet['artAnzahl'].strip()) if len(aDataSet['artAnzahl'].strip()) > 0 else None
+					aDataSet['artAbwBez'] = aDataSet['artAbwBez'].strip()
+					if not aDataSet['artId']:
+						aStatus = 'error'
+						error = '"artId" nicht vorhanden!'
+					else:
+						if mioeDB.tbl_vz_daten.objects.filter(id_vz_id=aVzId, id_art_id=aDataSet['artId'], id_mioe_ort_id=aMioeOrtId).count() != (1 if aDataSet['datenPk'] and aDataSet['datenPk'] > 0 else 0):
+							aStatus = 'error'
+							error = '"vz_daten" Anzahl stimmt nicht!'
+						else:
+							if aDataSet['datenPk'] and aDataSet['datenPk'] > 0:
+								aVzDatenModel = mioeDB.tbl_vz_daten.objects.get(pk=aDataSet['datenPk'], id_vz_id=aVzId, id_art_id=aDataSet['artId'], id_mioe_ort_id=aMioeOrtId)
+							else:
+								aVzDatenModel = mioeDB.tbl_vz_daten()
+							aVzDatenModel.id_vz_id = aVzId
+							aVzDatenModel.id_mioe_ort_id = aMioeOrtId
+							aVzDatenModel.id_art_id = aDataSet['artId']
+							aVzDatenModel.abw_bez = aDataSet['artAbwBez']
+							aVzDatenModel.anzahl = aDataSet['artAnzahl']
+							aVzDatenModel.save()
+							test = 'Gespeichert ...'
 			for aArtInVZ in mioeDB.tbl_art_in_vz.objects.filter(id_vz_id=aVzId):
 				aArtInVzMitDaten = {'model': aArtInVZ, 'daten': {'models': mioeDB.tbl_vz_daten.objects.filter(id_vz_id=aVzId, id_art_id=aArtInVZ.id_art.pk, id_mioe_ort_id=aMioeOrtId)}}
 				aArtenInVZ.append(aArtInVzMitDaten)
 		return httpOutput(json.dumps({
-			'success': 'success',
+			'status': aStatus,
+			'error': error,
 			'html': loader.render_to_string(
 				'mioedbvzmaske/vz_daten_formular.html',
 				RequestContext(request, {'aVz': aVz, 'aMioeOrt': aMioeOrt, 'aArtenInVZ': aArtenInVZ, 'vzDaten': vzDaten, 'test': test}),)}))
