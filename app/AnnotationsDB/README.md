29521efc2b56ca897b1fa8a6bb9d7dcddb4cc5ac
hcb79 <hcb@hcb.de>
2019-10-17 09:41:59 +0200

    AnnotationsDB -> Datenbank Models aktualisiert.

---
+++ app/AnnotationsDB/README.md
@@ -0,0 +1,55 @@
+# Annotations Tool
+
+## Anno-sent
+### Materialized View
+
+Muss manuell erstellt werden:
+
+```
+CREATE MATERIALIZED VIEW mat_adhocsentences
+  AS (
+  SELECT row_number() OVER (PARTITION BY true) as id, z.adhoc_sentence, z.tokenids, z.infid, z.transid, z.tokreih, z.seqsent, z.sentorig, z.sentorth, lag(z.senttext) OVER (order by z.adhoc_sentence asc) as left_context, z.senttext, lead(z.senttext) OVER (order by z.adhoc_sentence asc) as right_context, z.sentttlemma, z.sentttpos, z.sentsplemma, z.sentsppos, z.sentsptag, z.sentspdep, z.sentspenttype
+  FROM(
+  SELECT
+    y.adhoc_sentence,
+    ARRAY_AGG(y.id ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung asc) as tokenids,
+    "y"."ID_Inf_id" as infid,
+    "y"."transcript_id_id" as transid,
+    ARRAY_AGG(y.token_reihung ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung asc) as tokreih,
+    ARRAY_AGG(y.sequence_in_sentence ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung asc) as seqsent,
+    string_agg("text", ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentorig,
+    string_agg(CASE
+                  WHEN y.token_type_id_id = 1 AND y.fragment_of_id IS NOT NULL THEN NULL
+                  ELSE "text" END, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as senttext,
+    string_agg(CASE
+                  WHEN y.token_type_id_id = 1 AND y.fragment_of_id IS NOT NULL THEN NULL
+                  WHEN "ortho" IS NOT NULL THEN "ortho" ELSE "text" END, ' '
+                  ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentorth,
+    string_agg(y.ttlemma, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentttlemma,
+    string_agg(y.ttpos, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentttpos,
+    string_agg(y.splemma, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentsplemma,
+    string_agg(y.sppos, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentsppos,
+    string_agg(y.sptag, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentsptag,
+    string_agg(CASE WHEN y.spenttype = '' OR y.spenttype IS NULL THEN '#' ELSE y.spenttype END, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentspenttype,
+    string_agg(y.spdep, ' ' ORDER BY y."ID_Inf_id", y.transcript_id_id, y.token_reihung NULLS FIRST) as sentspdep
+      FROM
+      (SELECT *,count(is_start OR NULL) OVER (order by x."ID_Inf_id", x.transcript_id_id, x.token_reihung asc) as adhoc_sentence
+        FROM (
+        SELECT *,
+          CASE WHEN lag(t.token_type_id_id) Over (order by  t."ID_Inf_id", t.transcript_id_id, t.token_reihung asc) = 2 THEN TRUE ELSE FALSE end as is_start
+          from "AnnotationsDB_tbl_token" t
+          order by t."ID_Inf_id", t.transcript_id_id, t.token_reihung
+        ) x
+        ORDER BY x."ID_Inf_id", x.transcript_id_id, x.token_reihung
+      ) y
+      Group By adhoc_sentence, "y"."ID_Inf_id", "y"."transcript_id_id"
+		) z
+	);
+create unique index on mat_adhocsentences (id);
+```
+
+Vorhandene Materialized View löschen
+
+```
+DROP MATERIALIZED VIEW mat_adhocsentences
+```
--- app/AnnotationsDB/models.py
+++ app/AnnotationsDB/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.db import transaction, connection
+import datetime
+import time
 
 
 class tbl_event(models.Model):
-	start_time			= models.DurationField(																				  verbose_name="Start Zeit")
-	end_time			= models.DurationField(																				  verbose_name="End Zeit")
+	start_time			= models.DurationField(						  null=True												, verbose_name="Start Zeit")
+	end_time			= models.DurationField(						  null=True												, verbose_name="End Zeit")
 	layer				= models.IntegerField(						  null=True												, verbose_name="Layer")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
 
 	def __str__(self):
 		return "{} - {} bis {}".format(self.layer, self.start_time, self.end_time)
@@ -16,22 +21,35 @@ class tbl_event(models.Model):
 
 
 class tbl_token(models.Model):
+	text				= models.CharField(max_length=511																	, verbose_name="Das aktuelle Token")
 	token_type_id		= models.ForeignKey('tbl_token_type'		, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Token Type")
 	ortho				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Ortho")
-	lautg				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="lautgetreue Umschrift")
 	phon				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="phonetische Umschrift")
-	sonst				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="sonstige Umschrift")
-	transcomment		= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="kommentar zu Transkript")
 	ID_Inf				= models.ForeignKey('PersonenDB.tbl_informanten', blank=True, null=True	, on_delete=models.SET_NULL	, verbose_name="ID Informant")
 	fragment_of			= models.ForeignKey('tbl_token', related_name='rn_tbl_token_fragment_of', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Fragment von")
 	token_reihung		= models.IntegerField(						  null=True												, verbose_name="Token Reihung")
 	event_id			= models.ForeignKey('tbl_event', related_name='rn_tbl_token_event_id', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Event ID")
-	start_timepoint		= models.DurationField(																				  verbose_name="Start Zeitpunkt")
-	end_timepoint		= models.DurationField(																				  verbose_name="End Zeitpunkt")
+	start_timepoint		= models.DurationField(						  null=True												, verbose_name="Start Zeitpunkt")
+	end_timepoint		= models.DurationField(						  null=True												, verbose_name="End Zeitpunkt")
 	transcript_id		= models.ForeignKey('tbl_transcript'		, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Transcript ID")
 	likely_error		= models.BooleanField(default=False																	, verbose_name="Eventueller Fehler")
-	sentence			= models.IntegerField(						  blank=True, null=True									, verbose_name="Sentence ID")
+	sentence			= models.IntegerField(						  blank=True, null=True									, verbose_name="Sentence ID (delete!)")
+	sentence_id			= models.ForeignKey('KorpusDB.tbl_saetze'	, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Sentence ID")
 	sequence_in_sentence = models.IntegerField(						  null=True												, verbose_name="sequence_in_sentence")
+	text_in_ortho		= models.TextField(							  blank=True, null=True									, verbose_name="Text in Ortho")
+	lautg				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="lautgetreue Umschrift (delete!)")
+	sonst				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="sonstige Umschrift (delete!)")
+	transcomment		= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="kommentar zu Transkript (delete!)")
+	ttpos				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="TreeTagger POS")
+	ttlemma				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="TreeTagger Lemma")
+	ttcheckword			= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="TreeTagger Checkword")
+	sppos				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy POS")
+	sptag				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy Tag")
+	splemma				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy Lemma")
+	spdep				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy Dependency Relation")
+	sphead				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy Dependency Relation")
+	spenttype			= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Spacy Named Entity Type")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
 
 	def __str__(self):
 		return "\"{}\"".format(self.ortho)
@@ -39,11 +57,12 @@ class tbl_token(models.Model):
 	class Meta:
 		verbose_name = "Token"
 		verbose_name_plural = "Tokens"
-		ordering = ('sentence', 'token_reihung',)
+		ordering = ('sentence_id', 'token_reihung',)
 
 
 class tbl_token_type(models.Model):
 	token_type_name		= models.CharField(max_length=511																	, verbose_name="Token Typ Name")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
 
 	def __str__(self):
 		return "{}".format(self.token_type_name)
@@ -56,7 +75,9 @@ class tbl_token_type(models.Model):
 
 class tbl_transcript(models.Model):
 	name				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Name")
-	update_time			= models.DateTimeField(																				  verbose_name="Update Zeit")
+	update_time			= models.DateTimeField(auto_now_add=True															, verbose_name="Update Zeit")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
+	default_tier		= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="default_tier")
 
 	def __str__(self):
 		return "{} ({})".format(self.name, self.update_time)
@@ -67,9 +88,60 @@ class tbl_transcript(models.Model):
 		ordering = ('id',)
 
 
+class tbl_tier(models.Model):
+	tier_name			= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="Tier Name")
+	transcript_id		= models.ForeignKey('tbl_transcript'		, blank=True, null=True	, on_delete=models.SET_NULL		, verbose_name="Transcript ID")
+
+	def __str__(self):
+		return "{} -> {}".format(self.tier_name, self.transcript_id)
+
+	class Meta:
+		verbose_name = "Tier"
+		verbose_name_plural = "Tier"
+		ordering = ('transcript_id',)
+
+
+class tbl_event_tier(models.Model):
+	event_id			= models.ForeignKey('tbl_event'				, blank=True, null=True, on_delete=models.SET_NULL		, verbose_name="Event ID")
+	tier_id				= models.ForeignKey('tbl_tier'				, blank=True, null=True, on_delete=models.SET_NULL		, verbose_name="Tier ID")
+	ID_Inf				= models.ForeignKey('PersonenDB.tbl_informanten', blank=True, null=True, on_delete=models.SET_NULL	, verbose_name="ID Informant")
+	text				= models.CharField(max_length=511			, blank=True, null=True									, verbose_name="default_tier")
+
+	def __str__(self):
+		return "{} -> {} ({})".format(self.tier_id, self.event_id, self.ID_Inf)
+
+	class Meta:
+		verbose_name = "Event Tier"
+		verbose_name_plural = "Event Tier"
+		ordering = ('event_id',)
+
+
 class tbl_tokenset(models.Model):
 	id_von_token		= models.ForeignKey('tbl_token', related_name='rn_id_von_tbl_token', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Von Token ID")
 	id_bis_token		= models.ForeignKey('tbl_token', related_name='rn_id_bis_tbl_token', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="Bis Token ID")
+	updated				= models.DateTimeField(auto_now=True																				, verbose_name="Letztes Änderung")
+
+	def refreshCache():
+		dg = 0
+		for aTokenset in tbl_tokenset.objects.all():
+			aTokenset.save()
+			dg += 1
+		return dg
+
+	def save(self, *args, **kwargs):
+		# tbl_tokentoset_cache aktuallisieren ...
+		super().save(*args, **kwargs)
+		tbl_tokentoset_cache.objects.filter(id_tokenset=self.pk).delete()
+		if self.id_von_token and self.id_bis_token:
+			if self.id_von_token.ID_Inf.pk != self.id_bis_token.ID_Inf.pk:
+				raise Exception("self.id_von_token.ID_Inf.pk != self.id_bis_token.ID_Inf.pk")
+			if self.id_von_token.transcript_id.pk != self.id_bis_token.transcript_id.pk:
+				raise Exception("self.id_von_token.transcript_id.pk != self.id_bis_token.transcript_id.pk")
+			if self.id_von_token.token_reihung > self.id_bis_token.token_reihung:
+				(self.id_bis_token.token_reihung, self.id_von_token.token_reihung) = (self.id_von_token.token_reihung, self.id_bis_token.token_reihung)
+			with transaction.atomic():
+				for aToken in tbl_token.objects.values('pk').filter(ID_Inf=self.id_von_token.ID_Inf, transcript_id=self.id_von_token.transcript_id.pk, token_reihung__gte=self.id_von_token.token_reihung, token_reihung__lte=self.id_bis_token.token_reihung).order_by('token_reihung'):
+					tbl_tokentoset_cache(id_tokenset=self, id_token_id=aToken['pk']).save()
 
 	def __str__(self):
 		return "{} - {}".format(self.id_von_token, self.id_bis_token)
@@ -83,6 +155,7 @@ class tbl_tokenset(models.Model):
 class tbl_tokentoset(models.Model):
 	id_tokenset			= models.ForeignKey('tbl_tokenset'									, on_delete=models.CASCADE		, verbose_name="Tokenset")
 	id_token			= models.ForeignKey('tbl_token'										, on_delete=models.CASCADE		, verbose_name="Token")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
 
 	def __str__(self):
 		return "{} <- {}".format(self.id_tokenset, self.id_token)
@@ -91,3 +164,69 @@ class tbl_tokentoset(models.Model):
 		verbose_name = "Token to Token Set"
 		verbose_name_plural = "Token to Token Sets"
 		ordering = ('id_tokenset',)
+
+
+class tbl_tokentoset_cache(models.Model):
+	id_tokenset			= models.ForeignKey('tbl_tokenset'									, on_delete=models.CASCADE		, verbose_name="Tokenset")
+	id_token			= models.ForeignKey('tbl_token'										, on_delete=models.CASCADE		, verbose_name="Token")
+	updated				= models.DateTimeField(auto_now=True																, verbose_name="Letztes Änderung")
+
+	def __str__(self):
+		return "{} <- {}".format(self.id_tokenset, self.id_token)
+
+	class Meta:
+		verbose_name = "Token to Token Set Cache"
+		verbose_name_plural = "Token to Token Sets Cache"
+		ordering = ('id_tokenset',)
+
+
+class mat_adhocsentences(models.Model):
+	id					= models.AutoField(primary_key=True)
+	adhoc_sentence		= models.BigIntegerField(					  null=True												, verbose_name="adhoc_sentence")
+	tokenids			= ArrayField(models.IntegerField(			  null=True												, verbose_name="tokenids"))
+	infid				= models.IntegerField(						  null=True												, verbose_name="infid")
+	transid				= models.IntegerField(						  null=True												, verbose_name="transid")
+	tokreih				= ArrayField(models.IntegerField(			  null=True												, verbose_name="tokreih"))
+	seqsent				= ArrayField(models.IntegerField(			  null=True												, verbose_name="seqsent"))
+	sentorig			= models.TextField(							  blank=True, null=True									, verbose_name="sentorig")
+	sentorth			= models.TextField(							  blank=True, null=True									, verbose_name="sentorth")
+	left_context		= models.TextField(							  blank=True, null=True									, verbose_name="left_context")
+	senttext			= models.TextField(							  blank=True, null=True									, verbose_name="senttext")
+	right_context		= models.TextField(							  blank=True, null=True									, verbose_name="right_context")
+	sentttlemma			= models.TextField(							  blank=True, null=True									, verbose_name="sentttlemma")
+	sentttpos			= models.TextField(							  blank=True, null=True									, verbose_name="sentttpos")
+	sentsplemma			= models.TextField(							  blank=True, null=True									, verbose_name="sentsplemma")
+	sentsppos			= models.TextField(							  blank=True, null=True									, verbose_name="sentsppos")
+	sentsptag			= models.TextField(							  blank=True, null=True									, verbose_name="sentsptag")
+	sentspdep			= models.TextField(							  blank=True, null=True									, verbose_name="sentspdep")
+	sentspenttype		= models.TextField(							  blank=True, null=True									, verbose_name="sentspenttype")
+
+	class Meta:
+		db_table = "mat_adhocsentences"
+		managed = False
+		verbose_name = "mat_adhocsentences"
+		verbose_name_plural = "mat_adhocsentences"
+		ordering = ('adhoc_sentence',)
+
+
+class tbl_refreshlog_mat_adhocsentences(models.Model):
+	created_at			= models.DateTimeField(auto_now_add=True, db_index=True												, verbose_name="Erstellt")
+	duration			= models.DurationField(																				  verbose_name="Dauer")
+
+	def __str__(self):
+		return "{} ({})".format(self.created_at, self.duration)
+
+	@transaction.atomic
+	def refresh():
+		start_time = time.monotonic()
+		with connection.cursor() as cursor:
+			cursor.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY mat_adhocsentences")
+		end_time = time.monotonic()
+		tbl_refreshlog_mat_adhocsentences.objects.create(duration=datetime.timedelta(seconds=end_time - start_time))
+		return end_time - start_time
+
+	class Meta:
+		db_table = "tbl_refreshlog_mat_adhocsentences"
+		verbose_name = "tbl_refreshlog_mat_adhocsentences"
+		verbose_name_plural = "tbl_refreshlog_mat_adhocsentences"
+		ordering = ('created_at',)
