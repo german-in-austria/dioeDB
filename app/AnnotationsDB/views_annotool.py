b411be1949df5166422152b7d3216cc58ea5f2b8
hcb79 <hcb@hcb.de>
2020-02-05 18:10:36 +0100

    AnnotationsDB -> Test ...

--- app/AnnotationsDB/views_annotool.py
+++ app/AnnotationsDB/views_annotool.py
@@ -13,7 +13,7 @@ from copy import deepcopy
 import datetime
 
 spuren = [
-	{'title': 'text', 'field': ['t']},
+	{'title': 'text', 'field': ['t'], 'default': {}},
 	{'title': 'ortho', 'field': ['o'], 'default': {'text': ['o', 't']}},
 	{'title': 'text_in_ortho', 'field': ['to'], 'dontshow': True},
 	{'title': 'phon', 'field': ['ph']},
