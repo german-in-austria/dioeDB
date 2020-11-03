46ea2a64bff8919e4bca0a5ea8bd63d6734996cb
arnold-graf <arnold.graf@gmail.com>
2020-11-03 14:44:07 +0100

    try to add https support explicitly

--- app/dioeDB/settings.py
+++ app/dioeDB/settings.py
@@ -103,7 +103,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 # CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ('127.0.0.1:8000', 'localhost:8080', 'transcribe.dioe.at', 'dioedb.dioe.at', 'local-transcribe.dioe.at')
+CORS_ORIGIN_WHITELIST = [
+  'https://localhost:8080',
+  'http://localhost:8080',
+  '127.0.0.1:8000',
+  'transcribe.dioe.at',
+  'dioedb.dioe.at'
+]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = (
 	'DELETE',
