618691f7f7c4efc0cd33c1395f10272ce389ac80
hcb79 <hcb@hcb.de>
2020-11-11 18:10:41 +0100

    ignore Cookie manipulation in dev envroiment.

--- app/dioeDB/middleware.py
+++ app/dioeDB/middleware.py
@@ -3,8 +3,9 @@ cookies.Morsel._reserved["samesite"] = "SameSite"
 
 class SameSiteMiddleware:
   def process_response(self, request, response):
-    for name, value in response.cookies.items():
-      if not value.get("samesite"):
-          value["samesite"] = "None"
-          value["secure"] = True  # fixes plain set_cookie(name, value)
+    if request.get_host() != '127.0.0.1:8000':
+      for name, value in response.cookies.items():
+        if not value.get("samesite"):
+            value["samesite"] = "None"
+            value["secure"] = True  # fixes plain set_cookie(name, value)
     return response
