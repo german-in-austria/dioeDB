3b203ce03ed34a7ec0c2611759f1f20bfe5f7708
hcb79 <hcb@hcb.de>
2022-08-31 12:41:11 +0200

    DB -> Statistik -> Anzeige der Log mit Grafik für Transkripte in Übersicht.

--- app/DB/view_statistik.py
+++ app/DB/view_statistik.py
@@ -51,6 +51,9 @@ def view_statistik(request):
 			RequestContext(request, {'statistik': statistik, 'error': error, 'info': info}),
 		)
 	else:
+		transcripte = {}
+		for tr in adbmodels.transcript.objects.all():
+			transcripte[tr.id] = tr.name
 		statistikListe = []
 		tagBreite = 15
 		aDir = settings.AUDIO_ROOT
@@ -67,7 +70,8 @@ def view_statistik(request):
 			'ac': {},
 			'acs': {},
 			'c': len(statistikListe),
-			'cw': (len(statistikListe) - 1) * tagBreite + 2
+			'cw': (len(statistikListe) - 1) * tagBreite + 2,
+			'tr': {}
 		}
 		min = {}
 		max = {}
@@ -75,9 +79,8 @@ def view_statistik(request):
 		max2 = {}
 		for line in statistikListe:
 			for p in line:
-				print(p)
-			cleanLine = line[p]
-			cleanLine['lt'] = p[0:10] + '_' + p[10:]
+				cleanLine = line[p]
+				cleanLine['lt'] = p[0:10] + '_' + p[10:]
 			for p2 in cleanLine['kc']:
 				if p2 not in max or max[p2] < cleanLine['kc'][p2]:
 					max[p2] = cleanLine['kc'][p2]
@@ -105,7 +108,53 @@ def view_statistik(request):
 				av = ((line['ac'][c] - min2[c]) / (max2[c] - min2[c])) if max2[c] > min2[c] else 0.5
 				statistikGrafik['ac'][c].append(av)
 				statistikGrafik['acs'][c] += str(dg * tagBreite) + ',' + str((1 - av) * 100) + ' '
+			for tr in line['tr']:
+				if tr['i'] not in statistikGrafik['tr']:
+					statistikGrafik['tr'][tr['i']] = {
+						'n': transcripte[tr['i']] if tr['i'] in transcripte else 'None',
+						'i': tr['i'],
+						'fu': tr['u'],
+						'lu': tr['u'],
+						'p': '',
+						'fec': tr['e'],
+						'lec': tr['e'],
+						'dec': [tr['e']],
+						'minec': tr['e'],
+						'maxec': tr['e'],
+						'svgec': '',
+						'ftc': tr['t'],
+						'ltc': tr['t'],
+						'dtc': [tr['t']],
+						'mintc': tr['t'],
+						'maxtc': tr['t'],
+						'svgtc': ''
+					}
+				else:
+					statistikGrafik['tr'][tr['i']]['lu'] = tr['u']
+					statistikGrafik['tr'][tr['i']]['lec'] = tr['e']
+					statistikGrafik['tr'][tr['i']]['ltc'] = tr['t']
+					statistikGrafik['tr'][tr['i']]['dec'].append(tr['e'])
+					statistikGrafik['tr'][tr['i']]['dtc'].append(tr['t'])
+					if statistikGrafik['tr'][tr['i']]['minec'] > tr['e']:
+						statistikGrafik['tr'][tr['i']]['minec'] = tr['e']
+					if statistikGrafik['tr'][tr['i']]['maxec'] < tr['e']:
+						statistikGrafik['tr'][tr['i']]['maxec'] = tr['e']
+					if statistikGrafik['tr'][tr['i']]['mintc'] > tr['t']:
+						statistikGrafik['tr'][tr['i']]['mintc'] = tr['t']
+					if statistikGrafik['tr'][tr['i']]['maxtc'] < tr['t']:
+						statistikGrafik['tr'][tr['i']]['maxtc'] = tr['t']
 			dg = dg + 1
+		for k, gtr in statistikGrafik['tr'].items():
+			dge = dg - len(gtr['dec'])
+			for e in gtr['dec']:
+				av = ((e - gtr['minec']) / (gtr['maxec'] - gtr['minec'])) if gtr['maxec'] > gtr['minec'] else 0.5
+				gtr['svgec'] += str(dge * tagBreite) + ',' + str((1 - av) * 74 + 1) + ' '
+				dge = dge + 1
+			dgt = dg - len(gtr['dtc'])
+			for t in gtr['dtc']:
+				av = ((t - gtr['mintc']) / (gtr['maxtc'] - gtr['mintc'])) if gtr['maxtc'] > gtr['mintc'] else 0.5
+				gtr['svgtc'] += str(dgt * tagBreite) + ',' + str((1 - av) * 74 + 1) + ' '
+				dgt = dgt + 1
 		return render_to_response(
 			'DB/statistik_overview.html',
 			RequestContext(request, {'statistikData': statistikData, 'statistikListe': statistikListeClean, 'statistikLastLine': statistikListeClean[-1], 'statistikFirstLine': statistikListeClean[0], 'statistikGrafik': statistikGrafik, 'error': error, 'info': info}),
