afe95c6c6da546f501fc03aa10f6e705d1b87907
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-01-04 15:07:28 +0100

    Missed a dot for allowing a domain instead of a host

--- app/dioeDB/settings.py
+++ app/dioeDB/settings.py
@@ -57,7 +57,7 @@ if 'DIOEDB_DEBUG' in os.environ and (os.environ['DIOEDB_DEBUG'] == 'False' or os
 else:
 	DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '[::1]', '.dioe.at', 'acdh-ch-dev.oeaw.ac.at']
+ALLOWED_HOSTS = ['localhost', '[::1]', '.dioe.at', '.acdh-ch-dev.oeaw.ac.at']
 ALLOWED_CIDR_NETS = ['10.0.0.0/8', '127.0.0.0/8']
 
 SESSION_COOKIE_HTTPONLY = False
