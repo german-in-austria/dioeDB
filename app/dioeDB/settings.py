be8ac90b90c12b8ff0a424d5188dff458293592d
Jakob Bal <baljakob9@gmail.com>
2024-01-30 15:00:18 +0100

    Revert "Added rest framework swagger to requirements"
    This reverts commit 730151ae15565c018f0ccdf0089512732903038a.

--- app/dioeDB/settings.py
+++ app/dioeDB/settings.py
@@ -124,7 +124,6 @@ INSTALLED_APPS = (
     "AnnotationsDB",
     "DB",
     "mioeDB",
-    "rest_framework_swagger",
 )
 
 MIDDLEWARE_CLASSES = (
@@ -189,21 +188,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "dioeDB.wsgi.application"
-
-SWAGGER_SETTINGS = {
-    "exclude_url_names": [],
-    "exclude_namespaces": [],
-    "api_version": "0.1",
-    "api_path": "/",
-    "relative_paths": False,
-    "enabled_methods": ["get", "post", "put", "patch", "delete"],
-    "api_key": "",
-    "is_authenticated": False,
-    "is_superuser": False,
-    "unauthenticated_user": "django.contrib.auth.models.AnonymousUser",
-    "permission_denied_handler": None,
-    "resource_access_handler": None,
-}
 
 
 # Database
